5cb318f36fc40b928287022a641e5d902d192c74
Tennant <Tennant_1999@outlook.com>
2018-04-08 08:16:36 +0800

    upload utils

--- utils.py
+++ utils.py
@@ -8,4 +8,11 @@ class LossCalculator:
 		self.transform_loss_net = vgg.net(vgg.preprocess(stylized_image))
 
 	def content_loss(self, content_input_patch, content_layer, content_weight):
-		
+		content_loss_net = self.vgg.net(self.vgg.preprocess(content_input_patch))
+		return tf.nn.l2_loss(content_loss_net[content_layer] \
+			- self.transform_loss_net[content_layer]) / (_tensor_size(content_loss_net[content_layer]))
+
+
+def _tensor_size(tensor):
+	from operator import mul
+	return reduce(mul, (d.value for d in tensor.get_shape()), 1)
